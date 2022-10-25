bd63be0f7518c07f4f63fa8d302e41186ca7ce2d
Kozhan Nurassyl <nurasil21.07@mail.ru>
2022-10-25 20:28:25 +0600

    Adding necessary dependencies and menu button with title

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -7,11 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 
-    // BEST SDU students :
-    /*
-    * Makhmud, Omar, KNB, Magzhan
-    * */
-    // Come on
 }
