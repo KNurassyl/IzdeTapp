6b0b9d9bef3fa44967690e250c0ee252d4e905e6
Makhmud Assentay <200107067@stu.sdu.edu.kz>
2022-10-26 00:40:51 +0600

    displayed photo in app

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.izdetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.service)
 
+        val lost_found : ImageView = findViewById(R.id.lost_found)
+        val sell_buy : ImageView = findViewById(R.id.sell_buy)
+        val appartment : ImageView = findViewById(R.id.appa)
+
+        lost_found.setImageResource(R.drawable.lost_found)
+        sell_buy.setImageResource(R.drawable.sell_buy)
+        appartment.setImageResource(R.drawable.appartment)
+
+
+//        sell_buy.contentDescription = sell_buy.toString()
+//        appartment.contentDescription = appartment.toString()
+
+
     }
+
+
 }
