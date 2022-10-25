d4158309a85a919fe2d235a68e4953ace3b3e062
Kozhan Nurassyl <nurasil21.07@mail.ru>
2022-10-25 20:46:30 +0600

    working menu button

--- app/src/main/java/com/example/izdetapp/Convert.kt
+++ app/src/main/java/com/example/izdetapp/Convert.kt
@@ -3,17 +3,15 @@ package com.example.izdetapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.GravityCompat
+import com.example.izdetapp.R
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
 
-class MainActivity : AppCompatActivity() {
+class Convert : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerlayout)
         findViewById<View>(R.id.menuImg).setOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
     }
-
 }
