dff41f55261055c0650b992289ad0556a2c4042b
Makhmud Assentay <200107067@stu.sdu.edu.kz>
2022-10-26 11:51:36 +0600

    Combined Makhmud and Nurassyl

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.izdetapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
@@ -18,6 +19,14 @@ class MainActivity : AppCompatActivity() {
 
         val navigationView = findViewById<NavigationView>(R.id.navView)
         navigationView.itemIconTintList = null
+
+//        val lost_found : ImageView = findViewById(R.id.lost_found)
+//        val sell_buy : ImageView = findViewById(R.id.sell_buy)
+//        val appartment : ImageView = findViewById(R.id.appa)
+//
+//        lost_found.setImageResource(R.drawable.lost_found)
+//        sell_buy.setImageResource(R.drawable.sell_buy)
+//        appartment.setImageResource(R.drawable.appartment)
 
 
     }
