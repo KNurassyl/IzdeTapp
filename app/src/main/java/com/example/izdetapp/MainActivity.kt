2a8fb807c15471076bd115932ab1b3f014600491
Omar-projects-in-github <200107057@stu.sdu.edu.kz>
2022-10-28 09:41:50 +0600

    comments in main activity

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -2,37 +2,16 @@ package com.example.izdetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
+
         setContentView(R.layout.profile_activity)
-=======
-        setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerlayout)
-        findViewById<View>(R.id.menuImg).setOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
-
-        val navigationView = findViewById<NavigationView>(R.id.navView)
-        navigationView.itemIconTintList = null
-
-//        val lost_found : ImageView = findViewById(R.id.lost_found)
-//        val sell_buy : ImageView = findViewById(R.id.sell_buy)
-//        val appartment : ImageView = findViewById(R.id.appa)
-//
-//        lost_found.setImageResource(R.drawable.lost_found)
-//        sell_buy.setImageResource(R.drawable.sell_buy)
-//        appartment.setImageResource(R.drawable.appartment)
-
-
->>>>>>> dff41f55261055c0650b992289ad0556a2c4042b
     }
+    /*
+    I am coding here and someone is coding here
+    No we need to merge them
+     */
 
 }
