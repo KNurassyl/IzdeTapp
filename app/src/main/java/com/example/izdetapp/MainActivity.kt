bedf8e0c74b383fe7c0ffc0a98ea05b761bebce5
Omar-projects-in-github <200107057@stu.sdu.edu.kz>
2022-11-01 10:02:36 +0600

    dealing with problems

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -3,25 +3,9 @@ package com.example.izdetapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-/*
-changes in 31th October 2022
- */
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
-        asdfasdfasdf
-         */
-
-        setContentView(R.layout.profile_activity)
+        setContentView(R.layout.activity_profile)
     }
-    /*
-    I am coding here and someone is coding here
-    No we need to merge them
-     */
-
-    /*
-    some changes
-     */
 }
