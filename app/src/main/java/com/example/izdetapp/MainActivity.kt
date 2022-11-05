7e29cbb6e39bc207f27d008517edca8cafbc091d
Omar-projects-in-github <200107057@stu.sdu.edu.kz>
2022-11-06 00:28:27 +0600

    rolling back to the normal condition

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.izdetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.izdetapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_profile)
     }
 }
