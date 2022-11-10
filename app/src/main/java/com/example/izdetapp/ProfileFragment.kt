6b15a09d9822afc063159cb20c25d31a3d64bf88
Kozhan Nurassyl <nurasil21.07@mail.ru>
2022-11-10 21:07:59 +0600

    thinking about mvvm

---
+++ app/src/main/java/com/example/izdetapp/ProfileFragment.kt
@@ -0,0 +1,16 @@
+package com.example.izdetapp
+
+import android.content.ClipData.Item
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ProfileFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
