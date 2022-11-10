26c4d4d96d8a7dadf166234a28d6187f04d05058
Omar-projects-in-github <200107057@stu.sdu.edu.kz>
2022-11-10 21:28:59 +0600

    gradle MainActivity activity_main

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.izdetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.izdetapp.databinding.ActivityMainBinding
-
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_profile)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView5)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.basketFragment, R.id.createFragment,  R.id.messageFragment, R.id.pofileFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
