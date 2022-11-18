077f8987c52167a37947890c61c9d7ddffc5a3d7
Magzhan Abish <magzhan_abish@MacBook-Air-Magzhan.local>
2022-11-18 10:34:57 +0600

    JJJ

--- app/src/main/java/com/example/izdetapp/MainActivity.kt
+++ app/src/main/java/com/example/izdetapp/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.izdetapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.izdetapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +37,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragmentContainerView5)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.homeFragment, R.id.basketFragment,  R.id.messageFragment, R.id.profileFragment
+                R.id.homeFragment,
+                R.id.basketFragment,
+                R.id.messageFragment,
+                R.id.profileFragment,
+                R.id.logout
             )
         )
         binding.fab.setOnClickListener {
@@ -35,3 +54,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+
+
+
+
